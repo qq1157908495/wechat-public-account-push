ebdb786fec827981c9909eb05d112e6aba54a08f
qq1157908495 <100295073+qq1157908495@users.noreply.github.com>
2022-09-13 00:26:31 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -29,9 +29,9 @@ export const config = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "v-ljtAdRnuexqXIsppJrJVX1ztkUDz3-tLwnZx6SMB0",
       // 所在省份
-      province: "上海",
+      province: "上海市",
       // 所在城市
-       city: "徐汇区",
+       city: "上海市",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
